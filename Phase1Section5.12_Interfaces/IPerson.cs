773a80bfa30bb01401796bde0fade068e3b2afad
sureshreddyrayapati <resuresh0@gmail.com>
2024-01-10 18:02:44 +0530

    Add project files.

---
+++ Phase1Section5.12_Interfaces/IPerson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section5._12_Interfaces
+{
+    public interface IPerson
+    {
+        string getInfo();
+        string getTypeOfPerson();
+    }
+}
+
+
---
+++ Phase1Section5.12_Interfaces/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section5._12_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        public static void runApp()
+        {
+
+            Teacher teacher = new Teacher();
+            teacher.Name = "Mr.Teacher";
+            teacher.Address = "some address";
+            teacher.DateOfJoining = DateTime.Today;
+
+            Student student = new Student();
+            student.Name = "Student name";
+            student.Address = "some address";
+            student.DateOfJoining = DateTime.Today;
+
+            OfficeStaff staff = new OfficeStaff();
+            staff.Name = "Mr.Staff";
+            staff.Address = "some address";
+            staff.DateOfJoining = DateTime.Today;
+
+
+            Console.WriteLine(teacher.getInfo() + "\n" + teacher.getTypeOfPerson());
+            Console.WriteLine(student.getInfo() + "\n" + student.getTypeOfPerson());
+            Console.WriteLine(staff.getInfo() + "\n" + staff.getTypeOfPerson());
+        }
+    }
+}
